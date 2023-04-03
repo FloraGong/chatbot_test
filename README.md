7ccead320d8e8137725ea98736cc9d3da6072401
Zhang Dingwen <578328353@qq.com>
2023-04-03 15:16:34 +0800

    use another module to create chatbot

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# chatbot_test
-The project is a Q&A chatbot, which is based on LangChain and Gradio, The chatbot is able to answer questions about the dataset.
+# chatbot
+The project is a Q&A chatbot, which is based on LangChain and Streamlit, The chatbot is able to answer questions about the dataset.
 
 Put your own OpenAi key to use the chatbot.
--- qa.py
+++ qa.py
@@ -1,33 +1,50 @@
+import streamlit as st
+from streamlit_chat import message as st_message
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain import OpenAI, VectorDBQA
 from langchain.document_loaders import DirectoryLoader
-import gradio as gr
 import os
 
-loader = DirectoryLoader('data/', glob='**/*.txt')
+# 加载文档
+loader = DirectoryLoader('data', glob='**/*.txt')
 documents = loader.load()
+
+# 切分文本
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(documents)
-os.environ['OPENAI_API_KEY'] = '<your token>'
+
+# 获取文档嵌入
+os.environ[
+    'OPENAI_API_KEY'] = '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'
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
 docsearch = Chroma.from_documents(texts, embeddings)
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+
+# 创建 QA 模型
+qa = VectorDBQA.from_chain_type(llm=OpenAI(),
+                                chain_type="stuff",
+                                vectorstore=docsearch)
+# 创建历史记录
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+# Streamlit 应用程序
+st.title("问答机器人")
+st.write("您好！我是问答机器人。请问您有什么问题需要我回答吗？")
 
 
-def answer_question(question):
-    # 使用训练好的模型回答问题
-    answer = qa.run(question)
-    return answer
+# 构建回答
+def generate_answer():
+
+    user_message = st.session_state.input_text
+    message_bot = qa.run(user_message)
+
+    st.session_state.history.append({"message": user_message, "is_user": True})
+    st.session_state.history.append({"message": message_bot, "is_user": False})
 
 
-gradio_interface = gr.Interface(
-    fn=answer_question,
-    inputs="text",
-    outputs="text",
-    title="问答机器人",
-    description="输入您的问题，机器人将回答您的问题。"
-)
+st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
 
-gradio_interface.launch()
+for chat in st.session_state.history:
+    st_message(**chat)
